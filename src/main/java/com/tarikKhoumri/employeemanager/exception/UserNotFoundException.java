8dba8617c2e1cf915b37db43aa9e1e269eb6ab25
Tarik Khoumri <120420858+tarikkh@users.noreply.github.com>
2024-02-17 17:54:26 +0100

    Add files via upload

---
+++ src/main/java/com/tarikKhoumri/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.tarikKhoumri.employeemanager.exception;
+
+public class UserNotFoundException  extends  RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
